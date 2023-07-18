8094f866af21564a653912268bb0d491dd6d1246
dkarthur2 <davidarthur.tech@gmail.com>
2023-07-17 19:20:07 -0700

    print all caps and small alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	char small = 'a';
+	char caps = 'A';
 
+	while (small <= 'z')
+	{
+		putchar(small);
+		small++;
+	}
+	while (caps <= 'Z')
+	{
+		putchar(caps);
+		caps++;
+	}
+	putchar('\n');
+	return (0);
 }
