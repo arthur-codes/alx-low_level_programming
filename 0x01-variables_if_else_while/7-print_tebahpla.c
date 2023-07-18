9c1be06ed4ecae1fc767b527a902cdd2df14cca5
dkarthur2 <davidarthur.tech@gmail.com>
2023-07-17 20:00:48 -0700

    reverse alphabets

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
 
 }
