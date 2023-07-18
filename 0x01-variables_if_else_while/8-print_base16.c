5ae83e4f8a14538944bc33cfd5e4f6b298d9906e
dkarthur2 <davidarthur.tech@gmail.com>
2023-07-17 20:14:12 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int hexa;
 
+	for (hexa = '0'; hexa <= '9'; hexa++)
+	putchar(hexa);
+
+	for (hexa = 'a'; hexa <= 'f'; hexa++)
+	putchar(hexa);
+
+	putchar('\n');
+	return (0);
 }
