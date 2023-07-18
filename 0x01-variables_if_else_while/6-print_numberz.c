52c873ccd4c01ef1140541c24a5607a275a001f0
dkarthur2 <davidarthur.tech@gmail.com>
2023-07-17 19:53:39 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int n = 0;
 
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
 }
