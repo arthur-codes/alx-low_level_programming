4ade017f5cdecd1f140f17dd352dde0bddce610a
dkarthur2 <davidarthur.tech@gmail.com>
2023-07-27 19:28:02 -0700

    copy strings from source to destination

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncpy - A function to copy strings to a destination
+ *
+ * @dest: Destination parameter
+ *
+ * @src: source parameter
+ *
+ * @n: number of characters to be copied from source to destination
+ *
+ * Return: destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest + '\0');
+}
