6e812994fd37cc7169290faf0422942a871f33bf
dkarthur2 <davidarthur.tech@gmail.com>
2023-07-27 21:33:08 -0700

    string comps

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,12 @@
 int _strcmp(char *s1, char *s2)
 {
 	int result;
-	int i;
 
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
 	{
-		i++;
+		s1++;
+		s2++;
 	}
-	result = s1[i] - s2[i];
+	result = *s1 - *s2;
 	return (result);
 }
