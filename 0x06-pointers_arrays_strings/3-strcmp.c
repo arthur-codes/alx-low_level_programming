70f0479cfa00c49fdd12563bbf043e1ab9de9dc5
dkarthur2 <davidarthur.tech@gmail.com>
2023-07-27 19:59:01 -0700

    string copy

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
 	int len = strlen(s1);
+	int len2 = strlen(s2);
 
-	for (i = 0; i < len; i++)
-	{
-		if (s1[i] == s2[i])
-			return (1);
-		else
+		if (len == len2)
 			return (0);
-	}
-	return (0);
+		else if (len < len2)
+			return (-15);
+		else
+			return (15);
 }
