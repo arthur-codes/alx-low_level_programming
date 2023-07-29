c79e0895b24aa925c4167a8f3b7e200765c8d7f1
dkarthur2 <davidarthur.tech@gmail.com>
2023-07-28 18:21:00 -0700

    betty dey stress

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *rot13 - a function to rotate a character to it's 13 character
+ *
+ * @c: c parameter for testing
+ *
+ * Return: Return always
+ */
+char *rot13(char *c)
+{
+	int i, j, k;
+	char first_characters[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMn
+				   "NoOpPqQrRsStTuUvVwWxXyYzZ";
+	char second_characters[] = "nNoOpPqQrRsStTuUvVwWxXyYzZ
+				    "aAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	i = 0;
+	while (c[i] != '\0')
+	{
+		j = 0;
+		k = 0;
+		while (first_characters[j] != '\0')
+		{
+			if (c[i] == first_characters[j])
+			{
+				k = j;
+				c[i] = second_characters[k];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (c);
+}
