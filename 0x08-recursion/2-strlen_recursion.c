b59c7d0c0a32d8b3e687dcb8f9bf01715cae1946
dkarthur2 <davidarthur.tech@gmail.com>
2023-08-31 15:49:59 +0000

    third task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - prints the length of a string
+ *
+ * @s: parameter for testing
+ *
+ * Return: Always 1
+ */
+int _strlen_recursion(char *s)
+{
+	int n = strlen(s);
+
+	return (n);
+}
